3d2799cd8e898a1d05890c3fcf53e9ceb377375e
Marvin Thielk <mthielk@salk.edu>
2013-03-21 14:13:24 -0700

    Documentation for motion_signal.m
    added better documentation for the function motion_signal

--- Matlab/motion_signal.m
+++ Matlab/motion_signal.m
@@ -1,4 +1,39 @@
 function [str, flt_rsp1, flt_rsp2, flt_rsp1_shifted, flt_rsp2_shifted, spks_1_shifted, spks_2_shifted] = motion_signal(velocity, spks_1, spks_2, dx, trigger, trial_length, tau, tol)
+%MOTION_SIGNAL calculates the motion signal for a given velocity between two spike trains
+%   str = MOTION_SIGNAL(velocity, spks_1, spks_2, dx, trigger, trial_length, tau)
+%   calculates the motion signal strength for the given velocity where:
+%       spks_1 and spks_2 -> vertical vectors that contain all the spikes
+%                            times for the two neurons. 
+%       dx -> the distance between the two neurons (receptive fields)
+%       trigger -> the start time for the trial you are using
+%       trial_length -> length of the trial
+%       tau -> the tuning parameter to use.  The width of the gaussians in
+%              the motion signal algorithm
+%
+%   str = MOTION_SIGNAL(..., tol)
+%   same as above with:
+%       tol -> tolerance to use for the integration algorithm in matlab.
+%              Defaults to 1e-3
+%
+%   [str, flt_rsp1, flt_rsp2, flt_rsp1_shifted, flt_rsp2_shifted, ...
+%           spks_1_shifted, spks_2_shifted] = MOTION_SIGNAL(...)
+%   returns some info useful for plotting and analyzing where:
+%       flt_rsp1 and flt_rsp2 -> are function handles of the filtered
+%                                responses of the spike trains. (functions
+%                                of time best described over the interval
+%                                of 0 <= t <= trial_length)
+%       flt_rsp1_shifted and flt_rsp2_shifted -> are the funciton handles
+%                                of the filtered responses to the shifted
+%                                spike trains.
+%       spks_1_shifted and spks_2_shifted -> are the spike times of the
+%                                            shifted spikes (in vertical
+%                                            vectors)
+%
+%   For examples on usage see motion_script.m
+%
+% Marvin Thielk 2013
+% mthielk@salk.edu
+
 if nargin < 8
     AbsTol = 1e-4;
     RelTol = 1e-3;
