e13566ce5fe6b9fd6e32cfe9435cb3ed4cb832c3
Marvin Thielk <mthielk@salk.edu>
2013-04-01 08:29:30 -0700

    data_set selector
    added the data_set option to select the data set and set up the
    auto_set for the '2007-08-24-4' data set

--- Matlab/motion_script.m
+++ Matlab/motion_script.m
@@ -13,6 +13,8 @@
 % vary the options set in run_opt to turn different behaviors on and off
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % load -> reloads data
+% data_set -> sets the data set to do the analysis on.  built in support
+%             for '2007-03-27-1' and '2007-08-24-4'
 % remote -> if true uses data on the network, otherwise, local data
 % data_run -> which data run to load and analyze
 % cell_type -> string indicating which of the 4 cell populations to look at
@@ -53,17 +55,20 @@
 %                         fminunc to estimate the velocity.
 % data_run_plots -> same as trial_estimate but does it for each of the four
 %                   cell types
+% cell_types -> the cell types to include when running data_run_plots 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 run_opt.load = true; % T/F
+% run_opt.data_set = '2007-03-27-1';
+run_opt.data_set = '2007-08-24-4';
 run_opt.remote = true; % T/F
-run_opt.data_run = 13; % 12-19
+run_opt.data_run = 2; % 12-19
 run_opt.cell_type = 'On parasol'; % on/off parasol, on/off midget
 run_opt.config_num = 1; % 1-4
 run_opt.raster = false; % T/F
-run_opt.trial_raster = false; % T/F
-run_opt.trial_raster_shift = false; % T/F
-run_opt.manual_speed_tuning = false; % T/F
+run_opt.trial_raster = true; % T/F
+run_opt.trial_raster_shift = true; % T/F
+run_opt.manual_speed_tuning = true; % T/F
 run_opt.velocity_lim = 150; % >0
 run_opt.auto_speed_tuning = false; % T/F
 run_opt.tau = .01; % tuning parameter
@@ -75,50 +80,107 @@ run_opt.trial_estimate = false; % T/F
 run_opt.auto_set = true; % T/F -- note: overwrites run_opt params
 run_opt.trial_estimate_start = 120;
 run_opt.data_run_plots = true; % T/F
+run_opt.cell_types = {'Off midget', 'Off parasol', 'On midget', 'On parasol'};
 
 % just some presets so I don't have to change every param each time I
 % change the data_run
 if run_opt.auto_set
-    if run_opt.data_run == 12
-        run_opt.velocity_lim = 150;
-        run_opt.config_num = 1;
-        run_opt.trial_estimate_start = 110;
-        run_opt.tol = 1e-3;
-    elseif run_opt.data_run == 13
-        run_opt.velocity_lim = 300;
-        run_opt.config_num = 1;
-        run_opt.trial_estimate_start = 203;
-        run_opt.tol = 1e-4;
-    elseif run_opt.data_run == 14
-        run_opt.velocity_lim = 50;
-        run_opt.config_num = 1;
-        run_opt.trial_estimate_start = 14.6;
-        run_opt.tol = 1e-2;
-    elseif run_opt.data_run == 15
-        run_opt.velocity_lim = 150;
-        run_opt.config_num = 3;
-        run_opt.trial_estimate_start = 110;
-        run_opt.tol = 1e-3;
-    elseif run_opt.data_run == 16
-        run_opt.velocity_lim = 300;
-        run_opt.config_num = 1;
-        run_opt.trial_estimate_start = 203;
-        run_opt.tol = 1e-4;
-    elseif run_opt.data_run == 17
-        run_opt.velocity_lim = 50;
-        run_opt.config_num = 2;
-        run_opt.trial_estimate_start = 14.6;
-        run_opt.tol = 1e-2;
-    elseif run_opt.data_run == 18
-        run_opt.velocity_lim = 150;
-        run_opt.config_num = 1;
-        run_opt.trial_estimate_start = 110;
-        run_opt.tol = 1e-3;
-    elseif run_opt.data_run == 19
-        run_opt.velocity_lim = 300;
-        run_opt.config_num = 1;
-        run_opt.trial_estimate_start = 203;
-        run_opt.tol = 1e-4;
+    if strcmp(run_opt.data_set, '2007-03-27-1')
+        run_opt.cell_types = {'Off midget', 'Off parasol', 'On midget', 'On parasol'};
+        if run_opt.data_run == 12
+            run_opt.velocity_lim = 150;
+            run_opt.config_num = 1;
+            run_opt.trial_estimate_start = 110;
+            run_opt.tol = 1e-3;
+        elseif run_opt.data_run == 13
+            run_opt.velocity_lim = 300;
+            run_opt.config_num = 1;
+            run_opt.trial_estimate_start = 203;
+            run_opt.tol = 1e-4;
+        elseif run_opt.data_run == 14
+            run_opt.velocity_lim = 50;
+            run_opt.config_num = 1;
+            run_opt.trial_estimate_start = 14.6;
+            run_opt.tol = 1e-2;
+        elseif run_opt.data_run == 15
+            run_opt.velocity_lim = 150;
+            run_opt.config_num = 3;
+            run_opt.trial_estimate_start = 110;
+            run_opt.tol = 1e-3;
+        elseif run_opt.data_run == 16
+            run_opt.velocity_lim = 300;
+            run_opt.config_num = 1;
+            run_opt.trial_estimate_start = 203;
+            run_opt.tol = 1e-4;
+        elseif run_opt.data_run == 17
+            run_opt.velocity_lim = 50;
+            run_opt.config_num = 2;
+            run_opt.trial_estimate_start = 14.6;
+            run_opt.tol = 1e-2;
+        elseif run_opt.data_run == 18
+            run_opt.velocity_lim = 150;
+            run_opt.config_num = 1;
+            run_opt.trial_estimate_start = 110;
+            run_opt.tol = 1e-3;
+        elseif run_opt.data_run == 19
+            run_opt.velocity_lim = 300;
+            run_opt.config_num = 1;
+            run_opt.trial_estimate_start = 203;
+            run_opt.tol = 1e-4;
+        end
+    elseif strcmp(run_opt.data_set, '2007-08-24-4')
+        run_opt.cell_types = {'Off parasol', 'On midget', 'On parasol'};
+        if run_opt.data_run == 2
+            run_opt.velocity_lim = 150;
+            run_opt.config_num = 2;
+            run_opt.trial_estimate_start = 100;
+            run_opt.tol = 1e-3;
+        elseif run_opt.data_run == 3
+            run_opt.velocity_lim = 300;
+            run_opt.config_num = 3;
+            run_opt.trial_estimate_start = 200;
+            run_opt.tol = 1e-4;
+        elseif run_opt.data_run == 4
+            run_opt.velocity_lim = 300;
+            run_opt.config_num = 1;
+            run_opt.trial_estimate_start = 200;
+            run_opt.tol = 1e-4;
+        elseif run_opt.data_run == 5
+            run_opt.velocity_lim = 300;
+            run_opt.config_num = 2;
+            run_opt.trial_estimate_start = 200;
+            run_opt.tol = 1e-4;
+        elseif run_opt.data_run == 6
+            run_opt.velocity_lim = 150;
+            run_opt.config_num = 3;
+            run_opt.trial_estimate_start = 100;
+            run_opt.tol = 1e-3;
+        elseif run_opt.data_run == 7
+            run_opt.velocity_lim = 150;
+            run_opt.config_num = 1;
+            run_opt.trial_estimate_start = 100;
+            run_opt.tol = 1e-3;
+        elseif run_opt.data_run == 8
+            run_opt.velocity_lim = 50;
+            run_opt.config_num = 1;
+            run_opt.trial_estimate_start = 12.5;
+            run_opt.tol = 1e-2;
+        elseif run_opt.data_run == 9
+            run_opt.velocity_lim = 50;
+            run_opt.config_num = 1;
+            run_opt.trial_estimate_start = 12.5;
+            run_opt.tol = 1e-2;
+        elseif run_opt.data_run == 10
+            run_opt.velocity_lim = 150;
+            run_opt.config_num = 2;
+            run_opt.trial_estimate_start = 50;
+            run_opt.tol = 1e-2;
+        elseif run_opt.data_run == 11
+            run_opt.velocity_lim = 150;
+            run_opt.config_num = 1;
+            run_opt.trial_estimate_start = 50;
+            run_opt.tol = 1e-2;
+        end
     end
 end
 
@@ -131,14 +193,26 @@ if run_opt.load % load data fresh
 
     clear datarun tr
 
-    if run_opt.remote 
-        datarun{1}.names.rrs_params_path='/snle/analysis/2007-03-27-1/data011-nwpca/data011-nwpca.params';
-        datarun{2}.names.rrs_neurons_path=sprintf('/snle/analysis/2007-03-27-1/data%03d-from-data011-nwpca/data%03d-from-data011-nwpca.neurons', run_opt.data_run, run_opt.data_run);
-        datarun{2}.names.stimulus_path=sprintf('/braid/snle/analysis-archive/Experiments/Array/Analysis/2007-03-27-1/stimuli/s%d', run_opt.data_run);
-    else
-        datarun{1}.names.rrs_params_path='/Data/2007-03-27-1/data011-nwpca/data011-nwpca.params';
-        datarun{2}.names.rrs_neurons_path=sprintf('/Data/2007-03-27-1/data%03d-from-data011-nwpca/data%03d-from-data011-nwpca.neurons', run_opt.data_run, run_opt.data_run);
-        datarun{2}.names.stimulus_path=sprintf('/braid/snle/analysis-archive/Experiments/Array/Analysis/2007-03-27-1/stimuli/s%d', run_opt.data_run);
+    if strcmp(run_opt.data_set, '2007-03-27-1')
+        if run_opt.remote 
+            datarun{1}.names.rrs_params_path='/snle/analysis/2007-03-27-1/data011-nwpca/data011-nwpca.params';
+            datarun{2}.names.rrs_neurons_path=sprintf('/snle/analysis/2007-03-27-1/data%03d-from-data011-nwpca/data%03d-from-data011-nwpca.neurons', run_opt.data_run, run_opt.data_run);
+            datarun{2}.names.stimulus_path=sprintf('/braid/snle/analysis-archive/Experiments/Array/Analysis/2007-03-27-1/stimuli/s%02d', run_opt.data_run);
+        else
+            datarun{1}.names.rrs_params_path='/Data/2007-03-27-1/data011-nwpca/data011-nwpca.params';
+            datarun{2}.names.rrs_neurons_path=sprintf('/Data/2007-03-27-1/data%03d-from-data011-nwpca/data%03d-from-data011-nwpca.neurons', run_opt.data_run, run_opt.data_run);
+            datarun{2}.names.stimulus_path=sprintf('/braid/snle/analysis-archive/Experiments/Array/Analysis/2007-03-27-1/stimuli/s%02d', run_opt.data_run);
+        end
+    elseif strcmp(run_opt.data_set, '2007-08-24-4')
+        if run_opt.remote 
+            datarun{1}.names.rrs_params_path='/snle/analysis/2007-08-24-4/data001-nwpca/data001-nwpca.params';
+            datarun{2}.names.rrs_neurons_path=sprintf('/snle/analysis/2007-08-24-4/data%03d-from-data001-nwpca/data%03d-from-data001-nwpca.neurons', run_opt.data_run, run_opt.data_run);
+            datarun{2}.names.stimulus_path=sprintf('/braid/snle/analysis-archive/Experiments/Array/Analysis/2007-08-24-4/Stimuli/s%02d', run_opt.data_run);
+        else
+            datarun{1}.names.rrs_params_path='/Data/2007-08-24-4/data001-nwpca/data001-nwpca.params';
+            datarun{2}.names.rrs_neurons_path=sprintf('/Data/2007-08-24-4/data%03d-from-data001-nwpca/data%03d-from-data001-nwpca.neurons', run_opt.data_run, run_opt.data_run);
+            datarun{2}.names.stimulus_path=sprintf('/braid/snle/analysis-archive/Experiments/Array/Analysis/2007-08-24-4/Stimuli/s%02d', run_opt.data_run);
+        end
     end
     opt=struct('verbose',1,'load_params',1,'load_neurons',1,'load_obvius_sta_fits',true);
 
@@ -208,7 +282,7 @@ end
 
 if run_opt.trial_raster_shift
     k=1; kmin=1; kmax=length(tr); hk=loop_slider_n(k,kmin,kmax,1);
-    j=0; jmin=-200; jmax=-jmin; hj=loop_slider_n(j,jmin,jmax,2);
+    j=run_opt.trial_estimate_start; jmin=-run_opt.velocity_lim; jmax=run_opt.velocity_lim; hj=loop_slider_n(j,jmin,jmax,2);
     while true
         if ~ishandle(hk)
             break
@@ -228,7 +302,7 @@ if run_opt.manual_speed_tuning
     k=1; kmin=1; kmax=length(tr); hk=loop_slider_n(k,kmin,kmax,1);
     jmin=1; jmax=length(cell_indices2); j=round(.15 * jmax); hj=loop_slider_n(j,jmin,jmax,2);
     hmin=1; hmax=length(cell_indices2); h=round(.9 * hmax); hh=loop_slider_n(h,hmin,hmax,3);
-    v=15; vmin=-run_opt.velocity_lim; vmax=run_opt.velocity_lim; hv=loop_slider_n(v,vmin,vmax,4);
+    v=run_opt.trial_estimate_start; vmin=-run_opt.velocity_lim; vmax=run_opt.velocity_lim; hv=loop_slider_n(v,vmin,vmax,4);
     while true
         if ~ishandle(hk)
             break
@@ -365,9 +439,8 @@ if run_opt.data_run_plots
         matlabpool
     end
     options = optimset('Display', 'iter', 'TolFun', run_opt.tol, 'MaxFunEvals', 30, 'LargeScale', 'off');
-    cell_types = {'Off midget', 'Off parasol', 'On midget', 'On parasol'};
-    for j=1:length(cell_types)
-        cell_type = cell_types{j};
+    for j=1:length(run_opt.cell_types)
+        cell_type = run_opt.cell_types{j};
         cell_indices1=get_cell_indices(datarun{1},{cell_type});
         cell_indices2=get_cell_indices(datarun{2},{cell_type});
         
